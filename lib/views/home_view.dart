63d786bb1328ef2620ff9da87dca7160c8067d1d
Ziad Mohamed <ziad.mohamed51110@gmail.com>
2026-05-09 11:49:34 +0300

    create a notes list view

--- lib/views/home_view.dart
+++ lib/views/home_view.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:notes_app/views/widgets/custom_app_bar_widget.dart';
 import 'package:notes_app/views/widgets/note_card_widget.dart';
+import 'package:notes_app/views/widgets/notes_list_view.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
@@ -13,7 +14,7 @@ class HomeView extends StatelessWidget {
         child: Column(
           children: [
             CustomAppBar(),
-            Expanded(child: ListView(children: [NoteCardWidget()])),
+            Expanded(child: NotesListView()),
           ],
         ),
       ),
--- lib/views/widgets/note_card_widget.dart
+++ lib/views/widgets/note_card_widget.dart
@@ -5,59 +5,62 @@ class NoteCardWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: Color(0xffffcd7a),
-        borderRadius: BorderRadius.circular(16),
-      ),
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 10.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: Color(0xffffcd7a),
+          borderRadius: BorderRadius.circular(16),
+        ),
 
-      child: Column(
-        children: [
-          ListTile(
-            contentPadding: EdgeInsets.only(
-              left: 20,
-              top: 20,
-              bottom: 20,
-              right: 20,
-            ),
-            title: Padding(
-              padding: const EdgeInsets.only(bottom: 18.0),
-              child: Text(
-                "Flutter Tips",
-                style: TextStyle(color: Colors.black, fontSize: 26),
+        child: Column(
+          children: [
+            ListTile(
+              contentPadding: EdgeInsets.only(
+                left: 20,
+                top: 20,
+                bottom: 20,
+                right: 20,
               ),
-            ),
-
-            subtitle: Text(
-              "Build Your career with Ziad Mohamed",
-              style: TextStyle(
-                color: Color.fromARGB(122, 37, 27, 13),
-                fontSize: 16,
-              ),
-            ),
-            trailing: IconButton(
-              padding: EdgeInsets.only(bottom: 10, left: 25),
-              icon: Icon(Icons.delete, size: 32, color: Colors.black),
-              onPressed: () {},
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.only(right: 25.0, bottom: 20),
-            child: Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Text(
-                  textDirection: TextDirection.ltr,
-                  "May 21,2022",
-                  style: TextStyle(
-                    fontSize: 14,
-                    color: Color.fromARGB(122, 37, 27, 13),
-                  ),
+              title: Padding(
+                padding: const EdgeInsets.only(bottom: 18.0),
+                child: Text(
+                  "Flutter Tips",
+                  style: TextStyle(color: Colors.black, fontSize: 26),
                 ),
-              ],
+              ),
+
+              subtitle: Text(
+                "Build Your career with Ziad Mohamed",
+                style: TextStyle(
+                  color: Color.fromARGB(122, 37, 27, 13),
+                  fontSize: 16,
+                ),
+              ),
+              trailing: IconButton(
+                padding: EdgeInsets.only(bottom: 10, left: 25),
+                icon: Icon(Icons.delete, size: 32, color: Colors.black),
+                onPressed: () {},
+              ),
             ),
-          ),
-        ],
+            Padding(
+              padding: const EdgeInsets.only(right: 25.0, bottom: 20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    textDirection: TextDirection.ltr,
+                    "May 21,2022",
+                    style: TextStyle(
+                      fontSize: 14,
+                      color: Color.fromARGB(122, 37, 27, 13),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
